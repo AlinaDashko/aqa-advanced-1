f480e61f5ba071fb958949d488a3797c2c31c1d7
adashko <dashkoalina@yahoo.com>
2023-09-03 12:16:40 +0300

    deleted duplicated code for task5-3.js

--- js_basics/task5-3.js
+++ js_basics/task5-3.js
@@ -1,8 +1,3 @@
-let length = 25;
-let width = 15;
-let areaOfRectangle = length * width;
-console.log(areaOfRectangle);
-
 const radius = 10;
 const height = 30;
 const volume = Math.PI * Math.pow(radius, 2) * height;
