b117d29675cba21b899f1d8c18959d681fbb5024
adashko <dashkoalina@yahoo.com>
2023-09-05 21:19:01 +0300

    added homework for Conditions and Loops

---
+++ condition_and_loops/task3.js
@@ -0,0 +1,21 @@
+const number = 10;
+console.log("Таблиця множення за допомогою цикла for");
+for (let i = 1; i <= 10; i++) {
+    const result = number * i;
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+console.log("Таблиця множення за допомогою цикла while");
+let a = 1;
+while (a <= 10) {
+    let result = number * a;
+    console.log(number + " * " + a + " = " + result);
+    a++;
+}
+
+
+
+
+
+
+
