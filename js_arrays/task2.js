99b5ec3c58b331f636e127ab3be3c18b4cdc7413
adashko <dashkoalina@yahoo.com>
2023-09-16 22:48:43 +0300

    Revert "removed extra braces for task2.js"
    This reverts commit c2f446b340c24be00f72dae279062297085e2371.

--- js_arrays/task2.js
+++ js_arrays/task2.js
@@ -1,3 +1,3 @@
 const initialArray = [1, 2, 3, 4, 5];
-const mutationArray = initialArray.map((element, index) => element * index);
+const mutationArray = initialArray.map((element, index) => (element * index));
 console.log(mutationArray)
