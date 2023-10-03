121ffccc5728fa070ec5788f4f9a0f9374a59e25
adashko <dashkoalina@yahoo.com>
2023-10-03 15:40:57 +0300

    added homework for 9. Code style

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'google',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      rules: {
+        'require-jsdoc': 0,
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
